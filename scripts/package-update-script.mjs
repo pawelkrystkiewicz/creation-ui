34452f94eff5daea3667c2f0907445c9b0e4532e
Paweł <pawelkrystkiewicz@outlook.com>
2022-12-28 19:02:02 +0100

    chore: remove old deps lists

--- scripts/package-update-script.mjs
+++ scripts/package-update-script.mjs
@@ -1,55 +1,11 @@
 import path from 'path'
 import fs from 'fs'
 import pro from '../pro-packages.json' assert { type: 'json' }
-
-const devDependencies = {
-  '@creation-ui/core': '*',
-  '@babel/core': '^7.18.10',
-  '@babel/preset-react': '^7.18.6',
-  '@headlessui/react': '^1.7.0',
-  '@headlessui/tailwindcss': '^0.1.1',
-  '@tailwindcss/aspect-ratio': '^0.4.0',
-  '@tailwindcss/forms': '^0.5.2',
-  '@tailwindcss/line-clamp': '^0.4.0',
-  '@tailwindcss/typography': '^0.5.4',
-  '@types/node': '17.0.35',
-  '@types/react': '18.0.9',
-  '@types/react-dom': '18.0.5',
-  '@types/uuid': '^8.3.4',
-  autoprefixer: '^10.4.12',
-  'babel-loader': '^8.2.5',
-  'babel-plugin-module-resolver': '^4.1.0',
-  clsx: '^1.2.1',
-  postcss: '^8.4.16',
-  'postcss-loader': '^7.0.1',
-  react: '18.1.0',
-  'react-dom': '18.1.0',
-  'react-router-dom': '6',
-  'react-spring': '^9.5.5',
-  'react-use': '^17.4.0',
-  rollup: '^3.3.0',
-  'rollup-plugin-babel': '^4.4.0',
-  'rollup-plugin-filesize': '^9.1.2',
-  'rollup-plugin-peer-deps-external': '^2.2.4',
-  'rollup-plugin-postcss': '^4.0.2',
-  'rollup-plugin-progress': '^1.1.2',
-  'rollup-plugin-terser': '^7.0.2',
-  'rollup-plugin-visualizer': '^5.8.1',
-  sass: '^1.55.0',
-  'storybook-addon-next': '^1.6.7',
-  'storybook-addon-outline': '^1.4.2',
-  'storybook-addon-sass-postcss': '^0.1.3',
-  'storybook-addon-turbo-build': '^1.1.0',
-  'storybook-dark-mode': '^1.1.0',
-  tailwindcss: '^3.1.8',
-  'ts-loader': '^9.4.1',
-  typescript: '4.8.3',
-  uuid: '^8.3.2',
-}
+import mainPkg from '../package.json' assert { type: 'json' }
 
 const PRIVJS = 'https://r.privjs.com'
 const NPM = 'https://registry.npmjs.org'
-
+const version = mainPkg.version
 /**
  * 1.get all packages folders names
  * 2.for each folder create package.json path
@@ -74,6 +30,7 @@ const updateTailwindConfig = async pkgPath => {
 
 const updatePackageJson = async (name, pkgPath) => {
   const isPro = pro.includes(name)
+
   const pkg = JSON.parse(
     fs.readFileSync(path.join(pkgPath, 'package.json'), 'utf-8')
   )
@@ -82,10 +39,7 @@ const updatePackageJson = async (name, pkgPath) => {
     throw new Error(`@creation-ui/${name} package.json was not found!`)
   }
 
-  if (name !== 'core') {
-    pkg.devDependencies = devDependencies
-  }
-
+  pkg.version = version
   pkg.publishConfig = { registry: isPro ? PRIVJS : NPM }
   pkg.scripts['_publish'] = `npm publish --registry ${
     isPro ? PRIVJS : NPM
